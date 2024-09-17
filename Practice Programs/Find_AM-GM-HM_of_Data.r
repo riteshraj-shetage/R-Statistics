935ec9607dfeabce870891bad969e63a2a23f920
Riteshraj Shetage <144100040+rites-raj@users.noreply.github.com>
2024-09-17 18:04:32 +0530

    Update Find_AM-GM-HM_of_Data.r

--- Practice Programs/Find_AM-GM-HM_of_Data.r
+++ Practice Programs/Find_AM-GM-HM_of_Data.r
@@ -1,1 +1,21 @@
-#To find AM, GM, HM for ungrouped data
+# Program to find Arithmetic Mean, Geometric Mean, and Harmonic Mean of a list of numbers
+
+find_means <- function(nums) {
+  n <- length(nums)
+  
+  # Arithmetic Mean
+  AM <- sum(nums) / n
+  
+  # Geometric Mean
+  GM <- prod(nums)^(1/n)
+  
+  # Harmonic Mean
+  HM <- n / sum(1 / nums)
+  
+  return(list(AM = AM, GM = GM, HM = HM))
+}
+
+# Example
+nums <- as.numeric(unlist(strsplit(readline(prompt = "Enter numbers separated by spaces: "), " ")))
+means <- find_means(nums)
+cat("Arithmetic Mean:", means$AM, "\nGeometric Mean:", means$GM, "\nHarmonic Mean:", means$HM, "\n")
