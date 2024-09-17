6d819c87341cecb69908ada1571f0d3f5d145a7c
Riteshraj Shetage <144100040+rites-raj@users.noreply.github.com>
2024-09-17 18:07:55 +0530

    Update Reverse_given_Num.r

--- Practice Programs/Reverse_given_Num.r
+++ Practice Programs/Reverse_given_Num.r
@@ -1,1 +1,11 @@
-#Reverse a given Number
+# Program to reverse a given number
+
+reverse_number <- function(num) {
+  reversed_num <- as.numeric(paste(rev(unlist(strsplit(as.character(num), ""))), collapse = ""))
+  return(reversed_num)
+}
+
+# Example
+num <- as.integer(readline(prompt = "Enter a number: "))
+reversed_num <- reverse_number(num)
+cat("The reversed number is:", reversed_num, "\n")
