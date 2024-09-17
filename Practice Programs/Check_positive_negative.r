e7bd7452f8cdcec913f64c57c5426baecb00e166
Riteshraj Shetage <144100040+rites-raj@users.noreply.github.com>
2024-09-17 18:04:04 +0530

    Update Check_positive_negative.r

--- Practice Programs/Check_positive_negative.r
+++ Practice Programs/Check_positive_negative.r
@@ -1,1 +1,16 @@
-#To check whether the given integer is Positive or Negative
+# Program to check if a number is positive, negative or zero
+
+check_sign <- function(num) {
+  if (num > 0) {
+    return("Positive")
+  } else if (num < 0) {
+    return("Negative")
+  } else {
+    return("Zero")
+  }
+}
+
+# Example
+num <- as.integer(readline(prompt = "Enter a number: "))
+result <- check_sign(num)
+cat("The number", num, "is", result, "\n")
