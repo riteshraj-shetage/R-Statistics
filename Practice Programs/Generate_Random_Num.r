0062782bd9c6fcee6b1d17c3d91637b10c216817
Riteshraj Shetage <144100040+rites-raj@users.noreply.github.com>
2024-09-17 18:07:29 +0530

    Update Generate_Random_Num.r

--- Practice Programs/Generate_Random_Num.r
+++ Practice Programs/Generate_Random_Num.r
@@ -1,1 +1,12 @@
-#To generate random numbers from Discrete distn.s / Cont distn.s
+# Program to generate random numbers
+
+generate_random_numbers <- function(n, min_val, max_val) {
+  return(runif(n, min_val, max_val))
+}
+
+# Example
+n <- as.integer(readline(prompt = "Enter the number of random numbers to generate: "))
+min_val <- as.numeric(readline(prompt = "Enter the minimum value: "))
+max_val <- as.numeric(readline(prompt = "Enter the maximum value: "))
+random_numbers <- generate_random_numbers(n, min_val, max_val)
+cat("Random numbers:", random_numbers, "\n")
