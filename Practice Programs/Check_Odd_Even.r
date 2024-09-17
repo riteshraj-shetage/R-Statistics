03d7afec810304a5457e92d7f76724cbea2850f0
Riteshraj Shetage <144100040+rites-raj@users.noreply.github.com>
2024-09-17 18:03:08 +0530

    Update Check_Odd_Even.r

--- Practice Programs/Check_Odd_Even.r
+++ Practice Programs/Check_Odd_Even.r
@@ -1,1 +1,14 @@
-#Check if a number is Odd or Even
+# Program to check if a number is odd or even
+
+check_odd_even <- function(num) {
+  if (num %% 2 == 0) {
+    return("Even")
+  } else {
+    return("Odd")
+  }
+}
+
+# Example
+num <- as.integer(readline(prompt = "Enter a number: "))
+result <- check_odd_even(num)
+cat("The number", num, "is", result, "\n")
