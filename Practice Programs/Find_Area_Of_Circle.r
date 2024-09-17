a0777f114a50b8c6537ffee71994a05bff064690
Riteshraj Shetage <144100040+rites-raj@users.noreply.github.com>
2024-09-17 18:05:02 +0530

    Update Find_Area_Of_Circle.r

--- Practice Programs/Find_Area_Of_Circle.r
+++ Practice Programs/Find_Area_Of_Circle.r
@@ -1,1 +1,11 @@
+# Program to find the area of a circle
 
+find_area_circle <- function(radius) {
+  area <- pi * radius^2
+  return(area)
+}
+
+# Example
+radius <- as.numeric(readline(prompt = "Enter radius of the circle: "))
+area <- find_area_circle(radius)
+cat("The area of the circle is:", area, "\n")
