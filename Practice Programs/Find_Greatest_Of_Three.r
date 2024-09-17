740a101aa686de53fc6345d7f9ffec983e87fc8a
Riteshraj Shetage <144100040+rites-raj@users.noreply.github.com>
2024-09-17 18:05:29 +0530

    Update Find_Greatest_Of_Three.r

--- Practice Programs/Find_Greatest_Of_Three.r
+++ Practice Programs/Find_Greatest_Of_Three.r
@@ -1,1 +1,12 @@
-#To find greatest of three numbers
+# Program to find the greatest of three numbers
+
+find_greatest <- function(a, b, c) {
+  return(max(a, b, c))
+}
+
+# Example
+a <- as.integer(readline(prompt = "Enter the first number: "))
+b <- as.integer(readline(prompt = "Enter the second number: "))
+c <- as.integer(readline(prompt = "Enter the third number: "))
+greatest <- find_greatest(a, b, c)
+cat("The greatest number is:", greatest, "\n")
